55a3fc04ecf91c248b86fb3c7d00642d26fa2003
TURBOPC\ursau <uz2273@student.uni-lj.si>
2022-02-03 12:33:54 +0100

    Results for PIPR

--- combine_results.py
+++ combine_results.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-if __name__ == '__main__':
 
+def result1(print_table=True):
     # building table for result 1
 
     shs27_bfs = pd.read_csv('save_test_results/SHS27k_bfs', sep=',', header=None)
@@ -26,9 +26,12 @@ if __name__ == '__main__':
     stds.index = ['random', 'bfs', 'dfs','random', 'bfs', 'dfs']
 
     combined = means.round(2).astype(str) + ' pm ' + stds.round(2).astype(str)
-    print(combined.to_latex())
+    if print_table:
+        print(combined.to_latex())
+    return combined
 
-    # result 2
+
+def result2():
     gen_shs27_bfs = pd.read_csv('save_test_results/generalize_SHS27k_bfs', sep=',', header=None)
     gen_shs27_dfs = pd.read_csv('save_test_results/generalize_SHS27k_dfs', sep=',', header=None)
     gen_shs27_random = pd.read_csv('save_test_results/generalize_SHS27k_random', sep=',', header=None)
@@ -41,10 +44,12 @@ if __name__ == '__main__':
     gen_means = pd.DataFrame(columns=cols)
     gen_stds = pd.DataFrame(columns=cols)
 
-    gen_means = gen_means.append({'random': gen_shs27_random.mean().values[0], 'BFS': gen_shs27_bfs.mean().values[0],
-                                  'DFS': gen_shs27_dfs.mean().values[0]}, ignore_index=True)
-    gen_means = gen_means.append({'random': gen_shs148_random.mean().values[0], 'BFS': gen_shs148_bfs.mean().values[0],
-                                  'DFS': gen_shs148_dfs.mean().values[0]}, ignore_index=True)
+    gen_means = gen_means.append(
+        {'random': gen_shs27_random.mean().values[0], 'BFS': gen_shs27_bfs.mean().values[0],
+         'DFS': gen_shs27_dfs.mean().values[0]}, ignore_index=True)
+    gen_means = gen_means.append(
+        {'random': gen_shs148_random.mean().values[0], 'BFS': gen_shs148_bfs.mean().values[0],
+         'DFS': gen_shs148_dfs.mean().values[0]}, ignore_index=True)
     gen_stds = gen_stds.append({'random': gen_shs27_random.std().values[0], 'BFS': gen_shs27_bfs.std().values[0],
                                 'DFS': gen_shs27_dfs.std().values[0]}, ignore_index=True)
     gen_stds = gen_stds.append({'random': gen_shs148_random.std().values[0], 'BFS': gen_shs148_bfs.std().values[0],
@@ -57,7 +62,8 @@ if __name__ == '__main__':
     gen_combined = gen_means.round(2).astype(str) + ' pm ' + gen_stds.round(2).astype(str)
     print(gen_combined.to_latex())
 
-    # result 3
+
+def result3():
     gct_bfs_27 = pd.read_csv('save_test_results/all_GCT_SHS27k_bfs', sep=',', header=None)
     gct_dfs_27 = pd.read_csv('save_test_results/all_GCT_SHS27k_dfs', sep=',', header=None)
     gct_bfs_148 = pd.read_csv('save_test_results/all_GCT_SHS148k_bfs', sep=',', header=None)
@@ -68,8 +74,45 @@ if __name__ == '__main__':
 
     cols = ['SHS27k', 'SHS148k']
     res3 = pd.DataFrame(columns=cols)
+    combined = result1(False)
     rows = [combined.loc['bfs', 'X_all'].values, bfs, combined.loc['dfs', 'X_all'].values, dfs]
     for row in rows:
         res3 = res3.append({k: v for k, v in zip(cols, row)}, ignore_index=True)
 
     print(res3.to_latex())
+
+
+def results_pipr():
+    shs27_bfs = pd.read_csv('save_test_results/PIPR_SHS27k_bfs', sep=',', header=None).iloc[:, 3:]
+    shs27_dfs = pd.read_csv('save_test_results/PIPR_SHS27k_dfs', sep=',', header=None).iloc[:, 3:]
+    shs27_random = pd.read_csv('save_test_results/PIPR_SHS27k_random', sep=',', header=None).iloc[:, 3:]
+
+    shs148_bfs = pd.read_csv('save_test_results/PIPR_SHS148k_bfs', sep=',', header=None).iloc[:, 3:]
+    shs148_dfs = pd.read_csv('save_test_results/PIPR_SHS148k_dfs', sep=',', header=None).iloc[:, 3:]
+    shs148_random = pd.read_csv('save_test_results/PIPR_SHS148k_random', sep=',', header=None).iloc[:, 3:]
+
+    cols = ['X_all', 'X_bs', 'X_es', 'X_ns']
+    means = pd.DataFrame(columns=cols)
+    stds = pd.DataFrame(columns=cols)
+    for table in [shs27_random, shs27_bfs, shs27_dfs, shs148_random, shs148_bfs, shs148_dfs]:
+        table[3] = table[3].apply(lambda x: float(x.split(': ')[1]))
+        table[4] = table[4].replace(' None', -1)
+        means = means.append({k: v for k, v in zip(cols, table.mean())}, ignore_index=True)
+        stds = stds.append({k: v for k, v in zip(cols, table.std())}, ignore_index=True)
+
+    means.index = ['random', 'bfs', 'dfs', 'random', 'bfs', 'dfs']
+
+    stds.index = ['random', 'bfs', 'dfs', 'random', 'bfs', 'dfs']
+
+    combined = means.round(2).astype(str) + ' pm ' + stds.round(2).astype(str)
+    print(combined.to_latex())
+
+
+if __name__ == '__main__':
+    # call functions from above, depending on which results you would like to combine
+    # functions print skeletons for the latex tables used in the report
+    results_pipr()
+
+
+
+
